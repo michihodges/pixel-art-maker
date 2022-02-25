155e2ea8dd3a64d87e072a36524f5b4ccf38e89e
Michael Hodges <michihodges@Michaels-MacBook-Air-2.fritz.box>
2022-02-25 22:40:25 +0100

    feat: Further Comments

--- script.js
+++ script.js
@@ -13,17 +13,17 @@ function makeGrid() {
     // resets grid
     canvas.innerHTML = '';
     // creates grid
-    for (let row = 0; row < height; row++) { // loops over value of trs
-        const tableRow = document.createElement('tr'); // creates tr
-        for (let col = 0; col < width; col++) { // loops over value of tds
-            const tableData = document.createElement('td'); // creates td
-            tableRow.appendChild(tableData); // adds td
+    for (let row = 0; row < height; row++) { // loops over value of height input
+        const tableRow = document.createElement('tr'); // creates appropriate number of tr elements
+        for (let col = 0; col < width; col++) { // loops over value of width input
+            const tableData = document.createElement('td'); // creates appropriate number of td elements
+            tableRow.appendChild(tableData); // adds tds
             
             tableData.addEventListener('click', function respondToTheClick(e) { // listens for click event
                 console.log('A click was born.'); // logs successful click event
                 tableData.style.backgroundColor = color.value; // adds style of current color picked
             })
-        canvas.appendChild(tableRow); // adds tr
+        canvas.appendChild(tableRow); // adds trs
         }
     }
 }
